e9f1fbd5e4a0ce70c056084a29f2c7bd9a04bbb2
Sameer Ahmed <123456789+odin@users.noreply.github.com>
2025-03-31 11:28:35 +0530

    improve the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,34 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import "dotenv/config";
+import { NextFunction, Request, Response } from "express";
 
-interface UserPayload extends JwtPayload {
+interface AuthenticatedRequest extends Request {
   _id: string;
 }
 
-export const authentication = (req: any, res: any, next: any) => {
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(401).send({
-      msg: "Authentication token is missing",
+interface UserPayloadJwt extends JwtPayload {
+  _id: string;
+}
+
+export const authentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({
+        msg: "Authentication token is missing or invalid format",
+      });
+    }
+    const token = authHeader.split(" ")[1];
+    const verify = jwt.verify(token, process.env.JWT_SECRET!) as UserPayloadJwt;
+    (req as AuthenticatedRequest)._id = verify._id;
+    next();
+  } catch (e) {
+    return res.status(500).send({
+      msg: "Internal server error",
     });
   }
-  const verify = jwt.verify(token, process.env.JWT_SECRET!) as UserPayload;
-  if (!verify) {
-    return res.status(411).send({
-      msg: "User not authenticated",
-    });
-  }
-  req.id = verify._id;
-  next();
 };
